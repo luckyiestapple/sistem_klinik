eda30f51db62a05c38174148b5a97463687731cc
Yubasky <bayudoor1999@gmail.com>
2026-05-24 13:23:55 +0700

    ily

--- app/Views/rekam_medis/v_detail_rekmed.php
+++ app/Views/rekam_medis/v_detail_rekmed.php
@@ -147,17 +147,6 @@
               </div>
             </div>
 
-            <!-- Resep Obat -->
-            <h5 class="text-info border-bottom pb-1 mb-2 mt-3"><i class="la la-clipboard"></i> Resep Obat</h5>
-            <div class="row">
-              <div class="col-12 mb-2">
-                <div class="p-2 border border-warning rounded bg-light">
-                  <?= !empty($rekam_medis['resep_obat']) 
-                      ? nl2br(esc($rekam_medis['resep_obat'])) 
-                      : '<em class="text-muted">Tidak ada resep obat tertulis.</em>' ?>
-                </div>
-              </div>
-            </div>
 
             <!-- Tindak Lanjut -->
             <h5 class="text-info border-bottom pb-1 mb-2 mt-3"><i class="la la-calendar"></i> Rencana Tindak Lanjut</h5>
